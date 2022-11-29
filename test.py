7807342c5c22454f555c163d84600ac294b1f9c2
Lucokin <98477270+Lucokin@users.noreply.github.com>
2022-11-29 09:09:31 +0100

    add basic motor control
    motors will move one at a time with angles from user input

---
+++ test.py
@@ -0,0 +1,66 @@
+from Ax12 import Ax12
+
+#from kinematics
+
+# e.g 'COM3' windows or '/dev/ttyUSB0' for Linux
+Ax12.DEVICENAME = 'COM5'
+
+Ax12.BAUDRATE = 1_000_000
+
+# sets baudrate and opens com port
+Ax12.connect()
+
+# create AX12 instance with ID 2
+# motor 2 homeposition 420 blazin
+
+motor_id1 = 2
+my_dxl1 = Ax12(motor_id1)  
+my_dxl1.set_moving_speed(200)
+
+motor_id2 = 11
+my_dxl2 = Ax12(motor_id2)  
+my_dxl2.set_moving_speed(200)
+
+
+def user_input():
+    """Check to see if user wants to continue"""
+    ans = input('Continue? : y/n ')
+    if ans == 'n':
+        return False
+    else:
+        return True
+
+
+def main(motor_object1, motor_object2):
+    """ sets goal position based on user input """
+    bool_test = True
+    while bool_test:
+        # insert here the kinematocs integration
+        print("\nPosition of dxl ID: %d is %d " %
+            (motor_object1.id, motor_object1.get_present_position()))
+        # desired angle input
+        input_pos1 = int(input("goal pos: "))
+        #input_pos1 = 
+        motor_object1.set_goal_position(input_pos1)
+        print("Position of dxl ID: %d is now: %d " %
+            (motor_object1.id, motor_object1.get_present_position()))
+
+
+        print("\nPosition of dxl ID: %d is %d " %
+            (motor_object2.id, motor_object2.get_present_position()))
+        # desired angle input
+        input_pos2 = int(input("goal pos: "))
+        #input_po2 = 
+        motor_object2.set_goal_position(input_pos2)
+        print("Position of dxl ID: %d is now: %d " %
+            (motor_object2.id, motor_object2.get_present_position()))
+        bool_test = True
+
+# pass in AX12 object
+main(my_dxl1, my_dxl2)
+
+
+# disconnect
+my_dxl1.set_torque_enable(0)
+my_dxl2.set_torque_enable(0)
+Ax12.disconnect()
