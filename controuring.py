7f236f483220ffe16422222c3d7a0a78b25965d7
julianlcr <julian.leclerc@ecam.fr>
2022-12-06 08:43:43 +0100

    Contouring Update 2

--- controuring.py
+++ controuring.py
@@ -15,34 +15,44 @@ def contouring():
 
     cv2.waitKey(0)
 
-    #laplacian
+        #------------------------# laplacian #------------------------#
     #laplacian = cv2.Laplacian(grayscale,cv2.THRESH_BINARY)
-    
     #cv2.imshow("laplace",laplacian)
+    #cv2.waitKey(0)
     
-    #cv2.waitKey(0)
-    edges = cv2.Canny(grayscale,100,200)
-    cv2.imshow("edges",edges)
+        #------------------------# canny #------------------------#
     
-    #sobel
-    #cv2.waitKey(0)
-    #sobx = cv2.Sobel(grayscale,cv2.CV_64F,1,0,ksize=5)
-    #soby = cv2.Sobel(grayscale,cv2.CV_64F,0,1,ksize=5)
-    #sobel = cv2.addWeighted(sobx,0.3,soby,0.7,0)
-    #cv2.imshow("Added Image",sobel)
-
-    #cv2.waitKey(0)
-    #Threshold to binary image
-    #(thresh, im_bw) = cv2.threshold(edges, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU) #which determines the threshold automatically from the image using Otsu's method
-    #cv2.imshow('treshold', im_bw)
-
+    canny = cv2.Canny(grayscale,100,200)
+    cv2.imshow("edges",canny)
     cv2.waitKey(0)
+    
+        #------------------------# sobel #------------------------#
+        
+    #sobx = cv2.Sobel(grayscale,cv2.CV_64F,1,0,ksize=5)
+    #cv2.imshow("sobx",sobx)
+    #cv2.waitKey(0)
+    #soby = cv2.Sobel(grayscale,cv2.CV_64F,0,1,ksize=5)
+    #cv2.imshow("soby",soby)
+    #cv2.waitKey(0)
+    #sobel = cv2.add(sobx,soby)
+    #cv2.waitKey(0)
+    #cv2.imshow("Added Image",sobel)
+    #cv2.imwrite(r"C:\Users\julia\Documents\GitHub\2D---Plotter-Contourous-\ll.jpg", sobel) 
+    #sob = cv2.imread(r"C:\Users\julia\Documents\GitHub\2D---Plotter-Contourous-\ll.jpg")
+    #sob = cv2.cvtColor(sob,cv2.COLOR_BGR2GRAY)
+    #cv2.waitKey(0)
+    
+        #------------------------# threshold #------------------------#
+        
+    #(thresh, im_bw) = cv2.threshold(grayscale, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU) #which determines the threshold automatically from the image using Otsu's method
+    #cv2.imshow('treshold', im_bw)
+    #cv2.waitKey(0)
 
-    #FindContours
-    contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE) 
+        #------------------------# FindContours #------------------------#
+    contours, hierarchy = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE) 
     print("Number of Contours found = " + str(len(contours)))
 
-    #Show Contours
+        #------------------------# Show Contours #------------------------#
     for cnt in contours : 
 
         approx = cv2.approxPolyDP(cnt, 0.009 * cv2.arcLength(cnt, True), True) 
